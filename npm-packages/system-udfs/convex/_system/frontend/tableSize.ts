68f7a7419e2ef562da1579adf0d25e315faeb190
Arnold Trakhtenberg <arnold@convex.dev>
2024-03-27 15:46:53 -0700

    sizeOfAllTables system udf (#24059)
    GitOrigin-RevId: 8c1c08529b42ecc131758e99cd863670e1df2c94

--- npm-packages/system-udfs/convex/_system/frontend/tableSize.ts
+++ npm-packages/system-udfs/convex/_system/frontend/tableSize.ts
@@ -1,5 +1,7 @@
+import sum from "lodash/sum";
 import { queryGeneric } from "../secretSystemTables";
 import { v } from "convex/values";
+import { DatabaseReader } from "../../_generated/server";
 
 export default queryGeneric({
   args: { tableName: v.string() },
@@ -9,5 +11,28 @@ export default queryGeneric({
     }
     // internal queries don't show up now
     return await db.query(tableName).count();
+  },
+});
+
+export const sizeOfAllTables = queryGeneric({
+  args: {},
+  handler: async function allTableSizes({ db }): Promise<number> {
+    // Getting private system table here is OK because there are no args to this
+    // system UDF.
+    const tables = await ((db as any).privateSystem as DatabaseReader)
+      .query("_tables")
+      .filter((q) => q.eq(q.field("state"), "active"))
+      .collect();
+    const tablesWithoutSystemTables = tables
+      .map((table) => table.name)
+      .filter((tableName) => !tableName.startsWith("_"));
+
+    const tableCounts = Promise.all(
+      tablesWithoutSystemTables.map(async (tableName) => {
+        return await db.query(tableName as any).count();
+      }),
+    );
+
+    return sum(await tableCounts);
   },
 });
