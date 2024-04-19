get-convex/convex-backend

commit fe33239cfc9cd26a293916c179834a2e113e0e89

[cluster migration] run retention during migration (#24504)
GitOrigin-RevId: 9146f965fb2009492d0e06b04e2fcc105e6e840e

crates/database/src/retention.rs
@@ -623,7 +623,7 @@ impl<RT: Runtime> LeaderRetentionManager<RT> {
     /// entries is the number of index entries we found were expired, not
     /// necessarily the total we deleted or wanted to delete, though they're
     /// correlated.
-    pub(crate) async fn delete(
+    async fn delete(
         min_snapshot_ts: Timestamp,
         persistence: Arc<dyn Persistence>,
         rt: &RT,
@@ -688,6 +688,32 @@ impl<RT: Runtime> LeaderRetentionManager<RT> {
         min_snapshot_ts
             .pred()
             .map(|timestamp| (timestamp, total_expired_entries))
+    }
+
+    pub async fn delete_all_no_checkpoint(
+        mut cursor_ts: Timestamp,
+        min_snapshot_ts: Timestamp,
+        persistence: Arc<dyn Persistence>,
+        rt: &RT,
+        all_indexes: &BTreeMap<IndexId, (GenericIndexName<TableId>, IndexedFields)>,
+        retention_validator: Arc<dyn RetentionValidator>,
+    ) -> anyhow::Result<()> {
+        while cursor_ts.succ()? < min_snapshot_ts {
+            let (new_cursor_ts, _) = Self::delete(
+                min_snapshot_ts,
+                persistence.clone(),
+                rt,
+                cursor_ts,
+                all_indexes,
+                retention_validator.clone(),
+            )
+            .await?;
+            tracing::info!(
+                "custom index retention completed between ts {cursor_ts} and {new_cursor_ts}"
+            );
+            cursor_ts = new_cursor_ts;
+        }
+        Ok(())
     }
 
     #[try_stream(ok = (Timestamp, InternalDocumentId), error = anyhow::Error)]
@@ -1222,9 +1248,8 @@ impl<RT: Runtime> LeaderRetentionManager<RT> {
         Ok(())
     }
 
-    async fn get_checkpoint(
+    pub async fn get_checkpoint_no_logging(
         persistence: &dyn PersistenceReader,
-        snapshot_reader: Reader<SnapshotManager>,
         retention_type: RetentionType,
     ) -> anyhow::Result<Timestamp> {
         let key = match retention_type {
@@ -1239,21 +1264,27 @@ impl<RT: Runtime> LeaderRetentionManager<RT> {
             .map(ConvexValue::try_from)
             .transpose()?;
         let checkpoint = match checkpoint_value {
-            Some(ConvexValue::Int64(ts)) => {
-                let checkpoint = Timestamp::try_from(ts)?;
-                match retention_type {
-                    RetentionType::Document => log_document_retention_cursor_age(
-                        (*snapshot_reader.lock().latest_ts()).secs_since_f64(checkpoint),
-                    ),
-                    RetentionType::Index => log_retention_cursor_age(
-                        (*snapshot_reader.lock().latest_ts()).secs_since_f64(checkpoint),
-                    ),
-                }
-                checkpoint
-            },
+            Some(ConvexValue::Int64(ts)) => Timestamp::try_from(ts)?,
             None => Timestamp::MIN,
             _ => anyhow::bail!("invalid retention checkpoint {checkpoint_value:?}"),
         };
+        Ok(checkpoint)
+    }
+
+    async fn get_checkpoint(
+        persistence: &dyn PersistenceReader,
+        snapshot_reader: Reader<SnapshotManager>,
+        retention_type: RetentionType,
+    ) -> anyhow::Result<Timestamp> {
+        let checkpoint = Self::get_checkpoint_no_logging(persistence, retention_type).await?;
+        match retention_type {
+            RetentionType::Document => log_document_retention_cursor_age(
+                (*snapshot_reader.lock().latest_ts()).secs_since_f64(checkpoint),
+            ),
+            RetentionType::Index => log_retention_cursor_age(
+                (*snapshot_reader.lock().latest_ts()).secs_since_f64(checkpoint),
+            ),
+        }
         Ok(checkpoint)
     }
 

crates/database/src/index_worker.rs
@@ -925,18 +925,15 @@ impl<RT: Runtime> IndexWriter<RT> {
         let min_snapshot_ts = self.retention_validator.min_snapshot_ts().await?;
         let all_indexes = btreemap! { index_id => (index_name, indexed_fields) };
         // TODO(lee) add checkpointing.
-        let mut cursor_ts = backfill_begin_ts;
-        while cursor_ts.succ()? < min_snapshot_ts {
-            (cursor_ts, _) = LeaderRetentionManager::delete(
-                min_snapshot_ts,
-                self.persistence.clone(),
-                &self.runtime,
-                cursor_ts,
-                &all_indexes,
-                self.retention_validator.clone(),
-            )
-            .await?;
-        }
+        LeaderRetentionManager::delete_all_no_checkpoint(
+            backfill_begin_ts,
+            min_snapshot_ts,
+            self.persistence.clone(),
+            &self.runtime,
+            &all_indexes,
+            self.retention_validator.clone(),
+        )
+        .await?;
         Ok(())
     }
 }

crates/database/src/lib.rs
@@ -153,6 +153,8 @@ pub use self::{
     retention::{
         latest_retention_min_snapshot_ts,
         FollowerRetentionManager,
+        LeaderRetentionManager,
+        RetentionType,
     },
     snapshot_manager::{
         Snapshot,

crates/indexing/src/index_registry.rs
@@ -12,6 +12,7 @@ use common::{
         database_index::{
             DatabaseIndexState,
             DeveloperDatabaseIndexConfig,
+            IndexedFields,
         },
         DeveloperIndexConfig,
         IndexConfig,
@@ -403,6 +404,23 @@ impl IndexRegistry {
             .values()
             .chain(self.pending_indexes.values())
             .map(|index| index.metadata())
+    }
+
+    pub fn all_database_index_configs(
+        &self,
+    ) -> BTreeMap<IndexId, (TabletIndexName, IndexedFields)> {
+        self.all_indexes()
+            .filter_map(|index| {
+                let index_id = index.id().internal_id();
+                let index_name = index.name.clone();
+                match &index.config {
+                    IndexConfig::Database {
+                        developer_config, ..
+                    } => Some((index_id, (index_name, developer_config.fields.clone()))),
+                    IndexConfig::Search { .. } | IndexConfig::Vector { .. } => None,
+                }
+            })
+            .collect()
     }
 
     pub fn all_enabled_indexes(&self) -> Vec<ParsedDocument<TabletIndexMetadata>> {

commit 7de2464888bc7d86d932d7436e8e532ba2e4fb4f

fix retention checkpoint age metric on startup (#24706)
accidentally regressed by #24504

GitOrigin-RevId: 929258e7ff48d42cbbfb2ed46a9951fe4d6cc70b

crates/database/src/metrics.rs
@@ -410,6 +410,22 @@ pub fn log_document_retention_cursor_age(age_secs: f64) {
     log_gauge(&DOCUMENT_RETENTION_CURSOR_AGE_SECONDS, age_secs)
 }
 
+register_convex_gauge!(
+    RETENTION_MISSING_CURSOR_INFO,
+    "Index retention has no cursor"
+);
+pub fn log_retention_no_cursor() {
+    log_gauge(&RETENTION_MISSING_CURSOR_INFO, 1.0)
+}
+
+register_convex_gauge!(
+    DOCUMENT_RETENTION_MISSING_CURSOR_INFO,
+    "Document retention has no cursor"
+);
+pub fn log_document_retention_no_cursor() {
+    log_gauge(&DOCUMENT_RETENTION_MISSING_CURSOR_INFO, 1.0)
+}
+
 register_convex_counter!(
     RETENTION_SCANNED_DOCUMENT_TOTAL,
     "Count of documents scanned by retention",

crates/database/src/retention.rs
@@ -116,11 +116,13 @@ use crate::{
         latest_min_document_snapshot_timer,
         latest_min_snapshot_timer,
         log_document_retention_cursor_age,
+        log_document_retention_no_cursor,
         log_document_retention_scanned_document,
         log_retention_cursor_age,
         log_retention_documents_deleted,
         log_retention_expired_index_entry,
         log_retention_index_entries_deleted,
+        log_retention_no_cursor,
         log_retention_scanned_document,
         log_snapshot_verification_age,
         retention_advance_timestamp_timer,
@@ -1249,13 +1251,22 @@ impl<RT: Runtime> LeaderRetentionManager<RT> {
         retention_type: RetentionType,
     ) -> anyhow::Result<Timestamp> {
         let checkpoint = Self::get_checkpoint_no_logging(persistence, retention_type).await?;
-        match retention_type {
-            RetentionType::Document => log_document_retention_cursor_age(
-                (*snapshot_reader.lock().latest_ts()).secs_since_f64(checkpoint),
-            ),
-            RetentionType::Index => log_retention_cursor_age(
-                (*snapshot_reader.lock().latest_ts()).secs_since_f64(checkpoint),
-            ),
+        if checkpoint > Timestamp::MIN {
+            // Only log if the checkpoint has been written once, to avoid logging time since
+            // epoch when the instance is first starting up.
+            match retention_type {
+                RetentionType::Document => log_document_retention_cursor_age(
+                    (*snapshot_reader.lock().latest_ts()).secs_since_f64(checkpoint),
+                ),
+                RetentionType::Index => log_retention_cursor_age(
+                    (*snapshot_reader.lock().latest_ts()).secs_since_f64(checkpoint),
+                ),
+            }
+        } else {
+            match retention_type {
+                RetentionType::Document => log_document_retention_no_cursor(),
+                RetentionType::Index => log_retention_no_cursor(),
+            }
         }
         Ok(checkpoint)
     }