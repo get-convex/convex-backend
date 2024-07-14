972b7fe9efad5b038c1632f4faf2d1a80820f118
Nipunn Koorapati <nipunn@convex.dev>
2024-07-13 19:25:14 -0700

    Hide more stuff from rust stacktraces (#27878)
    This stuff is less useful when observing stacktraces
    
    GitOrigin-RevId: 0c4a57bc6c5f2b49328897ed9e9d2f8b87ae9498

--- crates/errors/src/lib.rs
+++ crates/errors/src/lib.rs
@@ -19,7 +19,17 @@ mod metrics;
 
 /// These module prefixes are default-hidden from stacktraces
 /// See https://docs.sentry.io/platforms/rust/configuration/options/#in-app-exclude
-pub static SENTRY_IN_APP_EXCLUDE: LazyLock<Vec<&'static str>> = LazyLock::new(|| vec!["tokio::"]);
+pub static SENTRY_IN_APP_EXCLUDE: LazyLock<Vec<&'static str>> = LazyLock::new(|| {
+    vec![
+        "axum::",
+        "futures_core::",
+        "futures_util::",
+        "hyper::",
+        "minitrace::",
+        "tokio::",
+        "tower::",
+    ]
+});
 
 /// ErrorMetadata object can be attached to an anyhow error chain via
 /// `.context(e /*ErrorMetadata*/)`. It is a generic object to be used
