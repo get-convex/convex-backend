a9bc5c7379314d268afe2cec30343f71a7d64224
Jordan Hunt <65152573+jordanhunt22@users.noreply.github.com>
2024-07-16 13:41:35 -0400

    [Document Retention] Decrease time in between batches for document retention (#27931)
    Increase the speed of document retention by 50% by decreasing the time in between runs from 30 mins -> 20 mins.
    
    GitOrigin-RevId: babb9fa224c127d75a540f81c7513dda7b4eca0a

--- crates/common/src/knobs.rs
+++ crates/common/src/knobs.rs
@@ -429,7 +429,7 @@ pub static RETENTION_FAIL_ALL_MULTIPLIER: LazyLock<usize> =
 pub static DOCUMENT_RETENTION_BATCH_INTERVAL_SECONDS: LazyLock<Duration> = LazyLock::new(|| {
     Duration::from_secs(env_config(
         "DOCUMENT_RETENTION_BATCH_INTERVAL_SECONDS",
-        30 * 60,
+        20 * 60,
     ))
 });
 
