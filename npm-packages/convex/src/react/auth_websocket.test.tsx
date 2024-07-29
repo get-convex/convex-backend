db344efb42882e5091e82b53b8b78e2de55e4fc4
Tom Ballinger <tom@convex.dev>
2024-07-29 16:27:36 -0700

    Disable websocket auth tests (#28466)
    GitOrigin-RevId: 2a16c67c5fa21995741eeb0251d679f1844a2c99

--- npm-packages/convex/src/react/auth_websocket.test.tsx
+++ npm-packages/convex/src/react/auth_websocket.test.tsx
@@ -16,8 +16,11 @@ const testReactClient = (address: string) =>
     unsavedChangesWarning: false,
   });
 
+// Disabled due to flakes in CI
+// https://linear.app/convex/issue/ENG-7052/re-enable-auth-websocket-client-tests
+
 // On Linux these can retry forever due to EADDRINUSE so run then sequentially.
-describe.sequential("auth websocket tests", () => {
+describe.sequential.skip("auth websocket tests", () => {
   // This is the path usually taken on page load after a user logged in,
   // with a constant token provider.
   test("Authenticate via valid static token", async () => {
