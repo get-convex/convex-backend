49445632fb3a86b60b658c6c636a4f7f0e1bb050
Jordan Hunt <65152573+jordanhunt22@users.noreply.github.com>
2024-08-20 13:26:39 -0700

    [Document Retention] Increase to 3000 documents scanned per run (#29001)
    Increase max scanned documents from 1500 -> 3000.
    
    GitOrigin-RevId: 585e83b669498158f431aeaedd675889cb3befc9

--- crates/common/src/knobs.rs
+++ crates/common/src/knobs.rs
@@ -449,7 +449,7 @@ pub static DOCUMENT_RETENTION_BATCH_INTERVAL_SECONDS: LazyLock<Duration> = LazyL
 /// there are a bunch of writes at single timestamp. Then, we go until there are
 /// no more writes at that timestamp.
 pub static DOCUMENT_RETENTION_MAX_SCANNED_DOCUMENTS: LazyLock<usize> =
-    LazyLock::new(|| env_config("DOCUMENT_RETENTION_MAX_SCANNED_DOCUMENTS", 1500));
+    LazyLock::new(|| env_config("DOCUMENT_RETENTION_MAX_SCANNED_DOCUMENTS", 3000));
 
 /// Size at which a search index will be queued for snapshotting.
 pub static SEARCH_INDEX_SIZE_SOFT_LIMIT: LazyLock<usize> =
