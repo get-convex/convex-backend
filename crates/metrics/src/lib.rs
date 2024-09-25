dce2e98bebdae7c1c44cf1b4e0ae03e7d3675a01
Nipunn Koorapati <nipunn@convex.dev>
2024-09-24 17:33:18 -0700

    Log new metric broken out by db_cluster (#30079)
    GitOrigin-RevId: 928037da8273b46c0f0e57ec60a0eedea56fe1f7

--- crates/metrics/src/lib.rs
+++ crates/metrics/src/lib.rs
@@ -59,7 +59,7 @@ fn initialize_server_version() -> String {
     let version_str = SERVER_VERSION
         .as_ref()
         .map(|v| v.to_string())
-        .unwrap_or_else(|| "0.0.0".to_owned());
+        .unwrap_or_else(|| "unknown".to_owned());
     INIT_VERSION_GAUGE.call_once(|| {
         CONVEX_BINARY_VERSIONS_TOTAL
             .with_label_values(&[&SERVICE_NAME, &version_str])
