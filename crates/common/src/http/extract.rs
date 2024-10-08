80fe4681211f274fc693b8e6be93e0689b88fdf9
Sujay Jayakar <sujayakar@convex.dev>
2024-10-07 22:09:32 -0400

    Add tracing spans for JSON request buffering and parsing times (#30323)
    Add tracing for the time it takes to load in a request into memory and parse it as JSON.
    
    GitOrigin-RevId: a3feb508b726a963e0dbc9f5d117828f97ae4418

--- crates/common/src/http/extract.rs
+++ crates/common/src/http/extract.rs
@@ -13,7 +13,13 @@ use axum::{
         Response,
     },
 };
+use bytes::Bytes;
 use errors::ErrorMetadata;
+use http::HeaderMap;
+use minitrace::{
+    future::FutureExt,
+    Span,
+};
 use serde::{
     de::DeserializeOwned,
     Serialize,
@@ -83,8 +89,9 @@ where
 
 pub struct Json<T>(pub T);
 
-/// Wrapper type around axum::Json that uses HttpResponseError instead
-/// of JsonRejection to make sure we get propper logging / error reporting.
+/// Fork of axum::Json that uses HttpResponseError instead of JsonRejection to
+/// make sure we get propper logging / error reporting and integrates with our
+/// tracing framework.
 #[async_trait]
 impl<S, T> FromRequest<S> for Json<T>
 where
@@ -94,14 +101,45 @@ where
     type Rejection = HttpResponseError;
 
     async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
-        #[allow(clippy::disallowed_types)]
-        let t = axum::Json::<T>::from_request(req, state)
+        if !json_content_type(req.headers()) {
+            let err = anyhow::anyhow!(ErrorMetadata::bad_request(
+                "BadJsonBody",
+                "Expected request with `Content-Type: application/json`",
+            ));
+            return Err(err.into());
+        }
+        let bytes = Bytes::from_request(req, state)
+            .in_span(Span::enter_with_local_parent("buffering_body"))
             .await
             .map_err(|e| {
                 anyhow::anyhow!(ErrorMetadata::bad_request("BadJsonBody", e.body_text()))
             })?;
-        Ok(Self(t.0))
+
+        let t = {
+            let _span = Span::enter_with_local_parent("parse_json");
+            #[allow(clippy::disallowed_types)]
+            axum::Json::<T>::from_bytes(&bytes)
+                .map_err(|e| {
+                    anyhow::anyhow!(ErrorMetadata::bad_request("BadJsonBody", e.body_text()))
+                })?
+                .0
+        };
+        Ok(Self(t))
     }
+}
+
+fn json_content_type(headers: &HeaderMap) -> bool {
+    let Some(content_type) = headers.get(http::header::CONTENT_TYPE) else {
+        return false;
+    };
+    let Ok(content_type) = content_type.to_str() else {
+        return false;
+    };
+    let Ok(mime) = content_type.parse::<::mime::Mime>() else {
+        return false;
+    };
+    mime.type_() == "application"
+        && (mime.subtype() == "json" || mime.suffix().map_or(false, |name| name == "json"))
 }
 
 impl<T> IntoResponse for Json<T>
