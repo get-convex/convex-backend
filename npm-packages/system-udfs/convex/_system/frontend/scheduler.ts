aabd79569eb6f9951f2348bfd43e9eb3e817deeb
Ari <ari@convex.dev>
2024-12-04 09:32:55 -0800

    add udfPath option to scheduler system udf (#31908)
    Similar to paginatedScheduledJobs, it's nice for this function to allow querying a single function's scheduler status.
    
    GitOrigin-RevId: e855936fecb74eb3fdd8c7dce1872fad431c75dd

--- npm-packages/system-udfs/convex/_system/frontend/scheduler.ts
+++ npm-packages/system-udfs/convex/_system/frontend/scheduler.ts
@@ -4,11 +4,24 @@ import { v } from "convex/values";
 export const nextScheduledJobTimestamp = queryPrivateSystem({
   args: {
     componentId: v.optional(v.union(v.string(), v.null())),
+    udfPath: v.optional(v.string()),
   },
-  handler: async function ({ db }): Promise<bigint | null> {
+  handler: async function ({ db }, { udfPath }): Promise<bigint | null> {
+    if (udfPath === undefined) {
+      const nextJob = await db
+        .query("_scheduled_jobs")
+        .withIndex("by_next_ts", (q) => q.gt("nextTs", null))
+        .filter((q) => q.eq(q.field("state"), { type: "pending" }))
+        .order("asc")
+        .first();
+      return nextJob?.nextTs ?? null;
+    }
+
     const nextJob = await db
       .query("_scheduled_jobs")
-      .withIndex("by_next_ts", (q) => q.gt("nextTs", null))
+      .withIndex("by_udf_path_and_next_event_ts", (q) =>
+        q.eq("udfPath", udfPath).gt("nextTs", null),
+      )
       .filter((q) => q.eq(q.field("state"), { type: "pending" }))
       .order("asc")
       .first();
