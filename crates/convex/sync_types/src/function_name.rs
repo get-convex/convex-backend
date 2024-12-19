bd429117b7e8f3fa0b2584a3fe3aa45df3361290
Geoffry Song <goffrie@convex.dev>
2024-12-19 15:24:52 -0800

    Store Box<str> in FunctionName (#32473)
    The stored string always has len==cap so we might as well represent that in the type system.
    
    GitOrigin-RevId: 74e5897cd2495f025d226ba881a32ad42b6429d5

--- crates/convex/sync_types/src/function_name.rs
+++ crates/convex/sync_types/src/function_name.rs
@@ -6,14 +6,14 @@ use std::{
 use crate::identifier::check_valid_identifier;
 
 #[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd, Hash, derive_more::Display)]
-pub struct FunctionName(String);
+pub struct FunctionName(Box<str>);
 
 impl FromStr for FunctionName {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         check_valid_identifier(s)?;
-        Ok(FunctionName(s.to_string()))
+        Ok(FunctionName(s.into()))
     }
 }
 
@@ -27,17 +27,17 @@ impl Deref for FunctionName {
 
 impl From<FunctionName> for String {
     fn from(function_name: FunctionName) -> Self {
-        function_name.0
+        function_name.0.into_string()
     }
 }
 
 impl FunctionName {
     pub fn default_export() -> Self {
-        Self("default".to_string())
+        Self("default".into())
     }
 
     pub fn is_default_export(&self) -> bool {
-        &self.0 == "default"
+        *self.0 == *"default"
     }
 }
 
