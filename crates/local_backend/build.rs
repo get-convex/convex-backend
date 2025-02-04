3091f84eb5b120a741026f260b93a967db7a4ad1
Gautam Gupta <gautam@convex.dev>
2025-02-04 11:37:42 -0800

    allow overriding vergen in docker builds (#34036)
    Vergen has somewhat annoying behavior in that when _not_ running in a git repo, it sets all git-generated env vars to VERGEN_IDEMPOTENT_VALUE to prevent compile-time panics in apps that use `env!`
    So the only way to get around that is to insist that it fails when a git repo can't be found, catch the error, and then manually set the values such that they'll be correct if present in the docker build environment, otherwise they'll fall back to "unknown".
    
    GitOrigin-RevId: d2b5967e0be039d808f9440c744f25e05fd00509

--- crates/local_backend/build.rs
+++ crates/local_backend/build.rs
@@ -1,10 +1,28 @@
 use vergen::EmitBuilder;
 
 fn main() -> anyhow::Result<()> {
-    // recompile when there's a new git hash for beacon.
-    EmitBuilder::builder()
+    // Recompile when there's a new git hash for beacon.
+    // This is a workaround for https://github.com/rustyhorde/vergen/issues/174
+    // In docker builds, we need a way to pass overrides to Vergen when there's no
+    // actual git repo. We'll try emitting as usual, then fall back to env vars
+    // that might have been set in the docker build before falling back to empty
+    // strings.
+    if EmitBuilder::builder()
         .git_sha(false)
         .git_commit_timestamp()
-        .emit()?;
+        .fail_on_error()
+        .emit()
+        .is_err()
+    {
+        println!("cargo:rerun-if-changed=build.rs");
+        println!(
+            "cargo:rustc-env=VERGEN_GIT_SHA={}",
+            option_env!("VERGEN_GIT_SHA").unwrap_or_else(|| "unknown")
+        );
+        println!(
+            "cargo:rustc-env=VERGEN_GIT_COMMIT_TIMESTAMP={}",
+            option_env!("VERGEN_GIT_COMMIT_TIMESTAMP").unwrap_or_else(|| "unknown")
+        );
+    }
     Ok(())
 }
