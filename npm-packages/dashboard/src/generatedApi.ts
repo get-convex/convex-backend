7a79e2f161de07af37372cba97fcbb6b97a549ea
Rebecca Wang <rebecca@convex.dev>
2025-04-09 16:52:36 -0700

    tell chef whether a team is disabled (#36335)
    GitOrigin-RevId: bee603569b2b894e021e0ba795b37fa7d7aca797

--- npm-packages/dashboard/src/generatedApi.ts
+++ npm-packages/dashboard/src/generatedApi.ts
@@ -1632,6 +1632,7 @@ export interface components {
             warningThresholdCents?: number | null;
         };
         GetTokenInfoResponse: {
+            isDisabled: boolean;
             /** Format: int64 */
             tokensQuota: number;
             /** Format: int64 */
