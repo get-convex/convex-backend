df84a109581e8cacafa1a7e2847d35d540df0787
Ari <ari@convex.dev>
2025-05-02 14:22:30 -0700

    dashboard: reset logs view on deployment change (#36947)
    GitOrigin-RevId: e721d4ab8585692f24a91c3931ee045f092b129d

--- npm-packages/dashboard-common/src/features/logs/components/LogsView.tsx
+++ npm-packages/dashboard-common/src/features/logs/components/LogsView.tsx
@@ -3,11 +3,15 @@ import { LoadingTransition } from "@ui/Loading";
 import { PageContent } from "@common/elements/PageContent";
 import { useNents } from "@common/lib/useNents";
 import { Logs } from "@common/features/logs/components/Logs";
+import { DeploymentInfoContext } from "@common/lib/deploymentContext";
+import { useContext } from "react";
 
 export function LogsView() {
   const { nents, selectedNent } = useNents();
+  const { useCurrentDeployment } = useContext(DeploymentInfoContext);
+  const deployment = useCurrentDeployment();
   return (
-    <PageContent>
+    <PageContent key={deployment?.id}>
       <DeploymentPageTitle title="Logs" />
       <LoadingTransition>
         {nents && <Logs nents={nents} selectedNent={selectedNent} />}
