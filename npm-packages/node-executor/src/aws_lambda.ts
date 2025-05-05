90a7b77cf327b850c1fa2797cee3ce13e6ed326b
Tom Ballinger <tom@convex.dev>
2025-05-05 16:08:44 -0700

    Don't keep Lambda VM active until all timers expire (#31390)
    Don't allow timers to prolong the active period of an AWS Lambda VM
    
    GitOrigin-RevId: 3afd25f1618d51aa3fab6e7ba93d4eaa04190395

--- npm-packages/node-executor/src/aws_lambda.ts
+++ npm-packages/node-executor/src/aws_lambda.ts
@@ -11,6 +11,24 @@ declare const awslambda: any;
 // eslint-disable-next-line  @typescript-eslint/no-unused-vars
 export const handler = awslambda.streamifyResponse(
   async (event: any, responseStream: Writable, context: Context) => {
+    // Using `streamifyResponse()` changes the behavior of the Lambda VM
+    // as compared to promise/async API (and actually makes it more like the
+    // original callback Lambda API): instead of always freezing after the
+    // promise of this function resolves it remaining active if there are any
+    // setTimeout timers, etc. pending.
+    //
+    // Setting `callbackWaitsForEmptyEventLoop` to false restores that behavior
+    // of freezing the Firecracker VM until the next function invocation at
+    // which point such scheduled functions may run.
+    //
+    // The main benefit of this is not being billed for 10 minutes of
+    // Lambda execution when the streamed response was returned long
+    // before that, often in a couple seconds.
+    //
+    // The downside is loss of isolation between invocations:
+    // timers may fire the next time the runtime is used.
+    context.callbackWaitsForEmptyEventLoop = false;
+
     setDebugLogging(true);
     await warmupPromise;
     event.requestId = context.awsRequestId;
