ab40815f83456265fd826b87dffa171a020c092c
Nipunn Koorapati <nipunn@convex.dev>
2025-06-12 13:24:37 -0700

    Make plan_id into an enum. (#38064)
    GitOrigin-RevId: 37eb9c8c870182bbd9a50707bb53417d67925674

--- crates/big_brain_private_api_types/src/types.rs
+++ crates/big_brain_private_api_types/src/types.rs
@@ -1,3 +1,13 @@
+use std::{
+    fmt::Display,
+    str::FromStr,
+};
+
+use anyhow::Context;
+use serde::{
+    Deserialize,
+    Serialize,
+};
 use tuple_struct::{
     tuple_struct_string,
     tuple_struct_u64,
@@ -11,11 +21,51 @@ tuple_struct_u64!(PartitionId);
 tuple_struct_string!(AccessToken);
 tuple_struct_string!(DeviceName);
 tuple_struct_string!(AppName);
-tuple_struct_string!(PlanId);
 tuple_struct_string!(ProjectSlug);
 tuple_struct_string!(ProjectName);
 tuple_struct_string!(TeamName);
 tuple_struct_string!(TeamSlug);
 tuple_struct_string!(ReferralCode);
-
 tuple_struct_string!(VercelTeamName);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum PlanId {
+    ConvexBase,
+    ConvexStarterPlus,
+    ConvexProfessional,
+}
+
+impl Display for PlanId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = serde_json::to_value(self).unwrap();
+        write!(f, "{}", s.as_str().unwrap())
+    }
+}
+
+impl FromStr for PlanId {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        serde_json::from_value(serde_json::Value::String(s.to_owned()))
+            .with_context(|| format!("Failed to parse plan id: {s}"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_plan_id_display() {
+        let plans = [
+            (PlanId::ConvexBase, "CONVEX_BASE"),
+            (PlanId::ConvexStarterPlus, "CONVEX_STARTER_PLUS"),
+            (PlanId::ConvexProfessional, "CONVEX_PROFESSIONAL"),
+        ];
+        for (plan, expected) in plans {
+            assert_eq!(plan.to_string(), expected);
+            assert_eq!(plan, expected.parse::<PlanId>().unwrap());
+        }
+    }
+}
