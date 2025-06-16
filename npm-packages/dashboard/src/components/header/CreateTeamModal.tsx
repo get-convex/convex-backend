aed64f8d42573d3516ea5683579e6f46cb2c575c
Nicolas Ettlin <nicolas@convex.dev>
2025-06-16 20:41:48 +0200

    Add a local dev hint on increasing the team limit (#38184)
    GitOrigin-RevId: 7136824ff81846962467dba7e119b59191fc4fe3

--- npm-packages/dashboard/src/components/header/CreateTeamModal.tsx
+++ npm-packages/dashboard/src/components/header/CreateTeamModal.tsx
@@ -1,13 +1,21 @@
 import { Modal } from "@ui/Modal";
+import { LocalDevCallout } from "@common/elements/LocalDevCallout";
+import { useProfile } from "api/profile";
 import { CreateTeamForm } from "./CreateTeamForm";
 
 export function CreateTeamModal({ onClose }: { onClose(): void }) {
+  const profile = useProfile();
+
   return (
     <Modal title="Create Team" onClose={onClose}>
       <p className="mb-5">
         Collaborate with your team members by creating a Convex Team.
       </p>
       <CreateTeamForm onClose={onClose} />
+      <LocalDevCallout
+        tipText="Tip: Run this to increase the number of teams you can create:"
+        command={`cargo run --bin big-brain-tool -- --dev grant-entitlement --member-entitlement max_teams 500 --member-id ${profile?.id ?? "{MEMBER_ID}"} --reason "local" --for-real`}
+      />
     </Modal>
   );
 }
