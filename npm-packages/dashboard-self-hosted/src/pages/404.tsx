81d37594a726cc37dc0d40058f81b7a547268094
Ari <ari@convex.dev>
2025-07-15 15:19:57 -0700

    dashboard: fix styling of 404 page (#39132)
    GitOrigin-RevId: 9eae6366d627c8604d8adfd60335c15e88421679

--- npm-packages/dashboard-self-hosted/src/pages/404.tsx
+++ npm-packages/dashboard-self-hosted/src/pages/404.tsx
@@ -3,7 +3,7 @@ import { Button } from "@ui/Button";
 export default function Custom404() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="flex gap-2 divide-x text-content-primary">
+      <div className="flex gap-2 text-content-primary">
         <h2>404</h2>
         <div className="flex items-center gap-1 pl-2">
           <p>This page could not be found.</p>
--- npm-packages/dashboard/src/pages/404.tsx
+++ npm-packages/dashboard/src/pages/404.tsx
@@ -7,7 +7,7 @@ export default function Custom404() {
   return (
     <div className="h-screen">
       <LoginLayout>
-        <div className="flex gap-2 divide-x text-content-primary">
+        <div className="flex gap-2 text-content-primary">
           <h2>404</h2>
           <div className="flex items-center gap-1 pl-2">
             {query.reason === "deployment_not_found" ? (
