2622ecf5490d680caf97aa2afd57ba27bdc0974c
Jayde Boardman <jayde.boardman@autorfp.ai>
2025-07-21 22:56:43 +1000

    chore: bump example repo to Nuxt v4

--- npm-packages/private-demos/quickstarts/nuxt/app/app.vue
+++ npm-packages/private-demos/quickstarts/nuxt/app/app.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import { api } from "@/convex/_generated/api";
-
+import { api } from "../convex/_generated/api";
 const { data: tasks } = useConvexQuery(api.tasks.get);
 </script>
 
 <template>
   <div>
     <h1>Tasks</h1>
-    <ul v-if="tasks">
+    <ul>
       <li v-for="task in tasks" :key="task._id">
-        <input :id="task._id" type="checkbox" :checked="task.isCompleted" />
-        <label :for="task._id">{{ task.text }}</label>
+        <span>{{ task.text }}</span>
       </li>
     </ul>
   </div>
--- npm-packages/private-demos/quickstarts/nuxt/nuxt.config.ts
+++ npm-packages/private-demos/quickstarts/nuxt/nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['convex-nuxt'],
   convex: {
-    url: process.env.CONVEX_URL,
+    url: process.env.CONVEX_URL
   }
 })
