f8cae5c5989207c25ddfbf23c956f33e61e2ba25
Nicolas Ettlin <nicolas@convex.dev>
2025-07-22 15:36:17 -0400

    Fix Inter import in docs (#39328)
    GitOrigin-RevId: 3f0058795ebfb4fc190a8254287f149241cd82f0

--- npm-packages/docs/src/theme/Root.jsx
+++ npm-packages/docs/src/theme/Root.jsx
@@ -2,6 +2,13 @@ import { logEvent } from "convex-analytics";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import Analytics from "../components/Analytics/Analytics";
 
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+
 function Root({ children }) {
   useEffect(() => {
     logEvent("view doc load", { path: location.pathname });
