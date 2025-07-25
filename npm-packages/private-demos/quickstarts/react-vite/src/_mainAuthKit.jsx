5cb758a3639dd459698ebeadf776113a662d1386
Nick Nisi <nick.nisi@workos.com>
2025-07-25 10:04:20 -0500

    fix package name and add CORS step

--- npm-packages/private-demos/quickstarts/react-vite/src/_mainAuthKit.jsx
+++ npm-packages/private-demos/quickstarts/react-vite/src/_mainAuthKit.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AuthKitProvider, useAuth } from "@workos-inc/authkit-react";
 import { ConvexReactClient } from "convex/react";
-import { ConvexProviderWithAuthKit } from "convex/react-authkit";
+import { ConvexProviderWithAuthKit } from "@convex-dev/workos";
 import "./index.css";
 import App from "./App.tsx";
 
