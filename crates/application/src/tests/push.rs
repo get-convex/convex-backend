0d293cc883a23a0d5303c7ab7a8766ded0ba2bca
Jordan Hunt <65152573+jordanhunt22@users.noreply.github.com>
2025-08-25 16:18:03 -0700

    [Node Migration] Add server side tests for pushing a new node version (#40258)
    Adds tests for configuring a new node version during pushes. The asserts that the version is persisted as expected.
    
    GitOrigin-RevId: 7494c9268ef81d3f83b66e6dcae16f122693624d

--- crates/application/src/tests/push.rs
+++ crates/application/src/tests/push.rs
@@ -1,7 +1,14 @@
 use common::knobs::MAX_USER_MODULES;
 use keybroker::Identity;
-use model::config::types::ConfigFile;
+use model::{
+    config::types::ConfigFile,
+    source_packages::{
+        types::NodeVersion,
+        SourcePackageModel,
+    },
+};
 use runtime::testing::TestRuntime;
+use value::TableNamespace;
 
 use crate::{
     deploy_config::{
@@ -78,5 +85,79 @@ async fn test_max_size_push_no_components(rt: TestRuntime) -> anyhow::Result<()>
             )
             .await?;
     }
+    Ok(())
+}
+
+#[convex_macro::test_runtime]
+async fn test_change_node_version(rt: TestRuntime) -> anyhow::Result<()> {
+    let application = Application::new_for_tests(&rt).await?;
+    let mut tx = application.begin(Identity::system()).await?;
+    let node_version = SourcePackageModel::new(&mut tx, TableNamespace::Global)
+        .get_latest()
+        .await?
+        .and_then(|p| p.node_version);
+    assert!(node_version.is_none());
+
+    application
+        .run_test_push(StartPushRequest {
+            admin_key: "".into(),
+            dry_run: false,
+            functions: "convex/".into(),
+            app_definition: AppDefinitionConfigJson {
+                definition: None,
+                dependencies: vec![],
+                schema: None,
+                functions: make_modules(),
+                udf_server_version: "1.3939.3939".into(),
+            },
+            component_definitions: vec![],
+            node_dependencies: vec![],
+            node_version: Some("22".to_string()),
+        })
+        .await?;
+
+    let mut tx = application.begin(Identity::system()).await?;
+    let node_version = SourcePackageModel::new(&mut tx, TableNamespace::Global)
+        .get_latest()
+        .await?
+        .and_then(|p| p.node_version);
+    assert_eq!(node_version, Some(NodeVersion::V22x));
+    Ok(())
+}
+
+#[convex_macro::test_runtime]
+async fn test_change_node_version_no_components(rt: TestRuntime) -> anyhow::Result<()> {
+    let application = Application::new_for_tests(&rt).await?;
+    let mut tx = application.begin(Identity::system()).await?;
+    let node_version = SourcePackageModel::new(&mut tx, TableNamespace::Global)
+        .get_latest()
+        .await?
+        .and_then(|p| p.node_version);
+    assert!(node_version.is_none());
+
+    application
+        .push_config_no_components(
+            Identity::system(),
+            ConfigFile {
+                auth_info: None,
+                functions: "convex/".into(),
+            },
+            make_modules()
+                .into_iter()
+                .map(|m| m.try_into().unwrap())
+                .collect(),
+            "1.3939.3939".parse().unwrap(),
+            None,
+            None,
+            Some(NodeVersion::V22x),
+        )
+        .await?;
+
+    let mut tx = application.begin(Identity::system()).await?;
+    let node_version = SourcePackageModel::new(&mut tx, TableNamespace::Global)
+        .get_latest()
+        .await?
+        .and_then(|p| p.node_version);
+    assert_eq!(node_version, Some(NodeVersion::V22x));
     Ok(())
 }
