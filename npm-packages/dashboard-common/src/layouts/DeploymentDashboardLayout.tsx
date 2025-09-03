7645706420f9ec89322fc600baa4b7ca782dd9e1
Jordan Hunt <65152573+jordanhunt22@users.noreply.github.com>
2025-09-03 15:47:00 -0700

    [Node Migration] Add banner for upgrade/configuration (#40569)
    For projects that are on node 18, we display the upgrade date and link to the docs where they can manually specify their node version.
    
    Must be merged after https://github.com/get-convex/convex/pull/40555 is pushed to funrun.
    
    <img width="1768" height="996" alt="Screenshot 2025-09-02 at 4 33 38 PM" src="https://github.com/user-attachments/assets/0132ad13-e03a-4b54-90db-b44caf29f27f" />
    
    GitOrigin-RevId: 17f0bd88589a43c8098b89406f0d6a9099976e1c

--- npm-packages/dashboard-common/src/layouts/DeploymentDashboardLayout.tsx
+++ npm-packages/dashboard-common/src/layouts/DeploymentDashboardLayout.tsx
@@ -125,6 +125,7 @@ export function DeploymentDashboardLayout({
     <FunctionsProvider>
       <div className="flex h-full grow flex-col overflow-y-hidden">
         <PauseBanner />
+        <NodeVersionBanner />
         <div className="flex h-full flex-col sm:flex-row">
           <Sidebar
             collapsed={!!collapsed}
@@ -194,6 +195,30 @@ function PauseBanner() {
       page.
     </div>
   );
+}
+
+function NodeVersionBanner() {
+  const nodeVersion = useQuery(udfs.node.version);
+  const usingNode18 = nodeVersion === "nodejs18.x";
+
+  if (usingNode18) {
+    return (
+      <div className="border-y bg-background-warning py-2 text-center text-content-warning">
+        This deployment is using Node 18 and will be automatically upgraded to
+        Node 20 on October 22, 2025. To manually configure the Node version,
+        visit the{" "}
+        <Link
+          href="https://docs.convex.dev/production/project-configuration#configuring-the-nodejs-version"
+          className="text-content-link hover:underline"
+        >
+          docs
+        </Link>
+        .
+      </div>
+    );
+  }
+
+  return null;
 }
 
 function EmbeddedConvexLogo({ collapsed }: { collapsed: boolean }) {
