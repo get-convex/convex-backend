865f17f0978e77d6c0675e20a4af90d2d721c583
Ian Macartney <ian@convex.dev>
2025-11-06 12:58:02 -0800

    ian/reference component api (#42813)
    - show off & exercise using ComponentApi type from all codegen paths
    
    GitOrigin-RevId: 1e6881f347c214e5b903087b1278d001a43e5992

--- npm-packages/private-demos/components-legacy-ts/convex/messages.ts
+++ npm-packages/private-demos/components-legacy-ts/convex/messages.ts
@@ -3,12 +3,12 @@ import { query } from "./_generated/server";
 import { components } from "./_generated/api";
 import { Doc } from "./_generated/dataModel";
 import { v } from "convex/values";
+import type { ComponentApi } from "../examples/waitlist@name-with-dashes/_generated/component.js";
+
+const waitlist = components.waitlist satisfies ComponentApi;
 
 export const list = query(async (ctx): Promise<Doc<"messages">[]> => {
-  const result = await ctx.runQuery(
-    components.waitlist.index.sayGoodbyeFromQuery,
-    {},
-  );
+  const result = await ctx.runQuery(waitlist.index.sayGoodbyeFromQuery, {});
   console.log(result);
   return await ctx.db.query("messages").collect();
 });
@@ -24,50 +24,40 @@ export const save = action({
   args: { message: v.string() },
   returns: v.string(),
   handler: async (ctx, { message }) => {
-    return ctx.runAction(components.waitlist.index.storeInFile, { message });
+    return ctx.runAction(waitlist.index.storeInFile, { message });
   },
 });
 
 export const componentTest = action(async (ctx) => {
   console.log("calling into component...");
-  const response = await ctx.runAction(
-    components.waitlist.index.repeatMessage,
-    {
-      message: "hello",
-      n: 3,
-    },
-  );
+  const response = await ctx.runAction(waitlist.index.repeatMessage, {
+    message: "hello",
+    n: 3,
+  });
   console.log("received response from component:", response);
   return response;
 });
 
 export const scheduleSendWaitlistMessage = mutation(async (ctx) => {
   console.log("scheduling message");
-  await ctx.scheduler.runAfter(
-    30 * 1000,
-    components.waitlist.index.scheduleMessage,
-    {},
-  );
+  await ctx.scheduler.runAfter(30 * 1000, waitlist.index.scheduleMessage, {});
   console.log(await ctx.db.system.query("_scheduled_functions").collect());
   return "scheduled";
 });
 
 export const testPartialRollback = mutation(async (ctx) => {
-  const initialResult = await ctx.runQuery(
-    components.waitlist.index.latestWrite,
-    {},
-  );
+  const initialResult = await ctx.runQuery(waitlist.index.latestWrite, {});
   console.log(initialResult);
-  await ctx.runMutation(components.waitlist.index.writeSuccessfully, {
+  await ctx.runMutation(waitlist.index.writeSuccessfully, {
     text: "hello",
   });
   try {
-    await ctx.runMutation(components.waitlist.index.writeThenFail, {
+    await ctx.runMutation(waitlist.index.writeThenFail, {
       text: "world",
     });
   } catch (e) {
     console.log("caught error", e);
   }
-  const result = await ctx.runQuery(components.waitlist.index.latestWrite, {});
+  const result = await ctx.runQuery(waitlist.index.latestWrite, {});
   console.log(result);
 });
