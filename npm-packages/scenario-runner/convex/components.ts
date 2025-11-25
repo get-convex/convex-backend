049493bfc3585d3ac9aad702cfa78aa6d26e9561
Nicolas Ettlin <nicolas@convex.dev>
2025-11-24 19:46:41 -0800

    load generator: fix scenarios (#43517)
    GitOrigin-RevId: ebac0d547d08638b426ebc86fdd1068e24990813

--- npm-packages/scenario-runner/convex/components.ts
+++ npm-packages/scenario-runner/convex/components.ts
@@ -1,22 +1,25 @@
+import { v } from "convex/values";
 import { components } from "./_generated/api";
 import { action, mutation, query } from "./_generated/server";
 
+export const CACHE_BREAKER_ARGS = { cacheBreaker: v.number() };
+
 export const componentQuery = query({
-  args: {},
+  args: CACHE_BREAKER_ARGS,
   handler: async (ctx) => {
     return await ctx.runQuery(components.counterComponent.public.load);
   },
 });
 
 export const componentMutation = mutation({
-  args: {},
+  args: CACHE_BREAKER_ARGS,
   handler: async (ctx) => {
     return await ctx.runMutation(components.counterComponent.public.increment);
   },
 });
 
 export const componentAction = action({
-  args: {},
+  args: CACHE_BREAKER_ARGS,
   handler: async (ctx) => {
     const count = Math.floor(Math.random() * 4) + 1;
     await ctx.runAction(components.counterComponent.public.reset, { count });
