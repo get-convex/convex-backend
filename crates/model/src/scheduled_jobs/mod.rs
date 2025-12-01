2ca99623508fee2a2c4c320c72fb0af889a20fa4
Emma Forman Ling <emma@convex.dev>
2025-12-01 11:11:13 -0800

    Stop double writing args to scheduled jobs table and scheduled job args table (#43579)
    Stop double writing args to the `_scheduled_jobs` and `_scheduled_job_args` tables. Just write to `_scheduled_job_args`.
    
    GitOrigin-RevId: 26035b9fa3ca147fb50f766c958699e4408706a1

--- crates/model/src/scheduled_jobs/mod.rs
+++ crates/model/src/scheduled_jobs/mod.rs
@@ -281,7 +281,6 @@ impl<'a, RT: Runtime> SchedulerModel<'a, RT> {
         };
         let scheduled_job = ScheduledJobMetadata::new(
             path.clone(),
-            args.clone(),
             args_id.developer_id,
             ScheduledJobState::Pending,
             // Don't set next_ts in the past to avoid scheduler incorrectly logging
@@ -324,7 +323,6 @@ impl<'a, RT: Runtime> SchedulerModel<'a, RT> {
                         let scheduled_ts = self.tx.begin_timestamp();
                         ScheduledJobMetadata::new(
                             path,
-                            args,
                             args_id.developer_id,
                             ScheduledJobState::Canceled,
                             None,
