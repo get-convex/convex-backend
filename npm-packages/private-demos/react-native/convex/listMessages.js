4f487ca1a54b8969ee87ac7bd2757ffb98105155
Reece Yang <7040416+reeceyang@users.noreply.github.com>
2026-01-12 17:45:29 -0800

    remove old react native demo and update qa instructions (#44993)
    GitOrigin-RevId: eb88d5e3911ca7fe2d996bcb67dac875211d5082

--- npm-packages/private-demos/react-native/convex/listMessages.js
+++
@@ -1,7 +0,0 @@
-import {query} from './_generated/server';
-
-export default query({
-  handler: async ({ db }) => {
-    return await db.query('messages').collect();
-  }
-});
--- npm-packages/private-demos/react-native/convex/sendMessage.js
+++
@@ -1,8 +0,0 @@
-import {mutation} from './_generated/server';
-
-export default mutation({
-  handler: async ({ db }, { body, author }) => {
-    const message = { body, author };
-    await db.insert('messages', message);
-  }
-});
