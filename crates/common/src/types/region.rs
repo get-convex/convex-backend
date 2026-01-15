af95a355d1a0c230fef59646c426ff4340d8cbd0
Nipunn Koorapati <nipunn@convex.dev>
2026-01-15 11:38:18 -0800

    Make region into a string. (#45115)
    GitOrigin-RevId: 3af27cc54cca3414bed0e79cfe66783f93b30574

--- crates/common/src/types/region.rs
+++ crates/common/src/types/region.rs
@@ -1,22 +1,3 @@
-use serde::{
-    Deserialize,
-    Serialize,
-};
+use tuple_struct::tuple_struct_string;
 
-#[derive(Serialize, Deserialize, Copy, Clone, Debug)]
-pub enum Region {
-    #[serde(rename = "aws-us-east-1")]
-    AwsUsEast1,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_region_serialization() {
-        let region = Region::AwsUsEast1;
-        let serialized = serde_json::to_string(&region).unwrap();
-        assert_eq!(serialized, "\"aws-us-east-1\"");
-    }
-}
+tuple_struct_string!(Region);
