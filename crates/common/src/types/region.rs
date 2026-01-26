5844656d930e821b24ad792506c139c56eba7a8f
Reece Yang <7040416+reeceyang@users.noreply.github.com>
2026-01-26 13:55:35 -0800

    make set_test_region_as_default idempotent (#45524)
    GitOrigin-RevId: 638d72a69db5d58aa3914f54e08079771b305f3e

--- crates/common/src/types/region.rs
+++ crates/common/src/types/region.rs
@@ -24,7 +24,16 @@ static RUNTIME_DEFAULT_REGION: OnceLock<RegionName> = OnceLock::new();
 pub fn set_test_region_as_default() -> anyhow::Result<()> {
     RUNTIME_DEFAULT_REGION
         .set(TEST_REGION_NAME.clone())
-        .map_err(|_| anyhow::anyhow!("Default region already set to test region"))
+        .or_else(|region| {
+            // Allow calling this multiple times if it's already set to the test region name
+            if region == *TEST_REGION_NAME {
+                Ok(())
+            } else {
+                Err(anyhow::anyhow!(
+                    "Default region already set to non-test region"
+                ))
+            }
+        })
 }
 
 /// Returns the default region for the current environment.
