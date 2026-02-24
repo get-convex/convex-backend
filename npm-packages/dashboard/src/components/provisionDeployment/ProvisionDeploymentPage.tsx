9232d75d9a456b826d3755cffa81eae9042b6489
Nicolas Ettlin <nicolas@convex.dev>
2026-02-23 16:16:01 -0800

    Provision deployment page: redirect to preexisting (#47074)
    The `/development` or `/production` pages of a project allow users to provision a new default dev/prod deployment.
    
    When accessing them in a project that already has such a deployment, the current behavior is bad:
    * if the team has a default region, we show an error saying that the deployment already exist, but we don't help the user find it
    * if the team doesn’t have a default region, we show the region form, and then only show the error when the user submits it
    
    Of course, this is user-unfriendly, so I changed it to immediately redirect to the appropriate deployment when it already exists.
    
    GitOrigin-RevId: 50deebf368dd20bba8567320d1829a8ec2e40ced

--- npm-packages/dashboard/src/components/provisionDeployment/ProvisionDeploymentPage.tsx
+++ npm-packages/dashboard/src/components/provisionDeployment/ProvisionDeploymentPage.tsx
@@ -1,6 +1,10 @@
 import { useCurrentTeam } from "api/teams";
 import { useCurrentProject } from "api/projects";
+import { useDeployments } from "api/deployments";
+import { useProfile } from "api/profile";
 import { useRouter } from "next/router";
+import { useEffect, useMemo } from "react";
+import { Loading } from "@ui/Loading";
 import { ProvisionDeploymentForm } from "./ProvisionDeploymentForm";
 
 export function ProvisionDeploymentPage({
@@ -15,8 +19,43 @@ export function ProvisionDeploymentPage({
   const projectId = project?.id ?? null;
   const projectURI = `/t/${team?.slug}/${projectSlug}`;
 
-  if (projectId === null) {
-    return null;
+  const { deployments } = useDeployments(projectId ?? undefined);
+  const member = useProfile();
+
+  const existingDeployment = useMemo(() => {
+    if (!deployments) return undefined;
+    if (deploymentType === "prod") {
+      return (
+        deployments.find(
+          (d) =>
+            d.kind === "cloud" && d.deploymentType === "prod" && d.isDefault,
+        ) ?? null
+      );
+    }
+    if (member === undefined) return undefined;
+    return (
+      deployments.find(
+        (d) =>
+          d.kind === "cloud" &&
+          d.deploymentType === "dev" &&
+          d.isDefault &&
+          d.creator === member?.id,
+      ) ?? null
+    );
+  }, [deployments, deploymentType, member]);
+
+  useEffect(() => {
+    if (existingDeployment) {
+      void router.replace(`${projectURI}/${existingDeployment.name}`);
+    }
+  }, [existingDeployment, projectURI, router]);
+
+  if (
+    projectId === null ||
+    deployments === undefined ||
+    existingDeployment === undefined
+  ) {
+    return <Loading />;
   }
 
   return (
