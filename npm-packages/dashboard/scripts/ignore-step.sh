fca85f031afa0715e71463e414f000dcd1b71949
Nicolas Ettlin <nicolas@convex.dev>
2026-03-05 19:35:06 -0800

    dashboard: improve `ignore-step` build script (#47779)
    GitOrigin-RevId: cc1671a3d151a024d634a49978fae938e07ff812

--- npm-packages/dashboard/scripts/ignore-step.sh
+++ npm-packages/dashboard/scripts/ignore-step.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-if [[ "${VERCEL_ENV}" == "production" ]] ; then
+if [[ "${VERCEL_GIT_COMMIT_REF}" =~ ^(mergify|tmp-mergify)/ ]] ; then
+    # Always skip temporary mergify branches.
+    echo "🛑 - Build canceled on mergify branch"
+    exit 0;
+elif [[ "${VERCEL_ENV}" == "production" ]] ; then
     # Proceed with the build
     echo "✅ - Build can proceed on production branch"
     exit 1;
@@ -8,6 +12,6 @@ elif [[ "${VERCEL_GIT_COMMIT_REF}" == "release" ]] ; then
     echo "✅ - Build can proceed on release branch"
     exit 1;
 else
-    # Only build if the dashboard, dashboard-common, or the design system has changed.
-    git diff HEAD^ HEAD --quiet . ../dashboard-common ../@convex-dev/design-system && echo "🛑 - Build canceled"
+    # Only build if dashboard packages or design system changed.
+    git diff HEAD^ HEAD --quiet . ../dashboard-common ../dashboard-storybook ../@convex-dev/design-system && echo "🛑 - Build canceled"
 fi
