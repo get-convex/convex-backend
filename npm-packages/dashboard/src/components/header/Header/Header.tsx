a773298e16bf7214b0c5d5968fe5711a9f2360ff
Nicolas Ettlin <nicolas@convex.dev>
2026-03-13 16:49:26 -0700

    dashboard: inline Convex logo SVG in `<Header />` (#48313)
    GitOrigin-RevId: bfbbeb1da8f4b1084912043bdb56703d78e3db49

--- npm-packages/dashboard/src/components/header/Header/Header.tsx
+++ npm-packages/dashboard/src/components/header/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
 import classNames from "classnames";
-import Image from "next/legacy/image";
 import Link from "next/link";
 import { SupportWidget, useSupportFormOpen } from "elements/SupportWidget";
 import { Portal } from "@headlessui/react";
@@ -74,12 +73,7 @@ export function Header({ children, logoLink = "/", user }: HeaderProps) {
             passHref
             className="flex min-h-[28px] min-w-[28px] rounded-full"
           >
-            <Image
-              src="/convex-logo-only.svg"
-              width="28"
-              height="28"
-              alt="Convex logo"
-            />
+            <ConvexLogo />
           </Link>
         </div>
         <div>{children}</div>
@@ -94,5 +88,54 @@ export function Header({ children, logoLink = "/", user }: HeaderProps) {
         {user && <UserMenu />}
       </div>
     </header>
+  );
+}
+
+function ConvexLogo() {
+  return (
+    <svg
+      width={28}
+      height={28}
+      viewBox="0 0 367 370"
+      style={{
+        fillRule: "evenodd",
+        clipRule: "evenodd",
+        strokeLinejoin: "round",
+        strokeMiterlimit: 2,
+      }}
+      aria-label="Convex logo"
+    >
+      <g transform="matrix(1,0,0,1,-129.225,-127.948)">
+        <g transform="matrix(4.16667,0,0,4.16667,0,0)">
+          <g transform="matrix(1,0,0,1,86.6099,107.074)">
+            <path
+              d="M0,-6.544C13.098,-7.973 25.449,-14.834 32.255,-26.287C29.037,2.033 -2.48,19.936 -28.196,8.94C-30.569,7.925 -32.605,6.254 -34.008,4.088C-39.789,-4.83 -41.69,-16.18 -38.963,-26.48C-31.158,-13.247 -15.3,-5.131 0,-6.544"
+              style={{
+                fill: "rgb(245,176,26)",
+                fillRule: "nonzero",
+              }}
+            />
+          </g>
+          <g transform="matrix(1,0,0,1,47.1708,74.7779)">
+            <path
+              d="M0,-2.489C-5.312,9.568 -5.545,23.695 0.971,35.316C-21.946,18.37 -21.692,-17.876 0.689,-34.65C2.754,-36.197 5.219,-37.124 7.797,-37.257C18.41,-37.805 29.19,-33.775 36.747,-26.264C21.384,-26.121 6.427,-16.446 0,-2.489"
+              style={{
+                fill: "rgb(141,37,118)",
+                fillRule: "nonzero",
+              }}
+            />
+          </g>
+          <g transform="matrix(1,0,0,1,91.325,66.4152)">
+            <path
+              d="M0,-14.199C-7.749,-24.821 -19.884,-32.044 -33.173,-32.264C-7.482,-43.726 24.112,-25.143 27.557,2.322C27.877,4.876 27.458,7.469 26.305,9.769C21.503,19.345 12.602,26.776 2.203,29.527C9.838,15.64 8.889,-1.328 0,-14.199"
+              style={{
+                fill: "rgb(238,52,47)",
+                fillRule: "nonzero",
+              }}
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
   );
 }
