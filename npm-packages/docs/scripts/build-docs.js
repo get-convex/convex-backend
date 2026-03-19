1dee7b548f4745635ee7ee869cb8299817be15b4
Nicolas Ettlin <nicolas@convex.dev>
2026-03-18 17:03:57 -0700

    docs: add route to get the currently deployed version (#48568)
    GitOrigin-RevId: 0f6fdf691d53f6be93f0ea64b8eefd5267952d42

--- npm-packages/docs/scripts/build-docs.js
+++ npm-packages/docs/scripts/build-docs.js
@@ -1,4 +1,5 @@
 const { execSync } = require("child_process");
+const fs = require("fs");
 
 async function buildDocs() {
   console.log("📁 Cleaning docs/api directory...");
@@ -12,6 +13,11 @@ async function buildDocs() {
 
   console.log("📋 Copying redirects file...");
   execSync("npx copyfiles _redirects build/", { stdio: "inherit" });
+
+  console.log("🔖 Writing git commit SHA...");
+  const sha = execSync("git rev-parse HEAD").toString().trim();
+  fs.mkdirSync("build/api", { recursive: true });
+  fs.writeFileSync("build/api/version", JSON.stringify({ sha }));
 }
 
 buildDocs();
