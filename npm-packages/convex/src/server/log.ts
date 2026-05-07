dba85b8a2bef90575c86cfa456d7802b872a4f61
Reece Yang <7040416+reeceyang@users.noreply.github.com>
2026-05-07 16:33:28 -0700

    jsdoc for log.audit (#51397)
    GitOrigin-RevId: 4debaf5e1fdf7d4cdffbec4a72fdf53c7e95d47f

--- npm-packages/convex/src/server/log.ts
+++ npm-packages/convex/src/server/log.ts
@@ -3,6 +3,27 @@ import { vars } from "./logVars.js";
 
 // Type annotations are needed for the `unique symbol` types in `vars` to typecheck correctly
 interface Log {
+  /**
+   * Emit a durable audit log. This functionality is only available for Convex
+   * Enterprise (see https://www.convex.dev/enterprise/pricing).
+   *
+   * Use dynamic variables from `log.vars` to include deferred values that will
+   * be resolved when emitting the log. Cached query hits will replay audit logs
+   * with updated values.
+   * ```ts
+   * await log.audit({
+   *   action: "document.viewed",
+   *   actor: { userId },
+   *   source: {
+   *     ip: log.vars.ip,
+   *     userAgent: log.vars.userAgent,
+   *   },
+   *   timestamp: log.vars.now,
+   * });
+   * ```
+   *
+   * The log body must be JSON-serializable.
+   */
   audit: typeof audit;
   vars: typeof vars;
 }
