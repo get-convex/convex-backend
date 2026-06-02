3b831bcb2ad00091d8bd2851c6f92c65c4927614
Nicolas Ettlin <nicolas@convex.dev>
2026-06-01 18:30:43 -0700

    Update Vitest to v4.1.0 (#52547)
    GitOrigin-RevId: 387b56cd060083d15279796ebf034282283f7ac4

--- npm-packages/system-udfs/vitest.config.ts
+++ npm-packages/system-udfs/vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
@@ -6,5 +6,6 @@ export default defineConfig({
     watch: false,
     environment: "edge-runtime",
     server: { deps: { inline: ["convex-test"] } },
+    exclude: [...configDefaults.exclude, "**/dist/**"],
   },
 });
