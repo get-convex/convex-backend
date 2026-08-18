9b6a15cf28c350d690a3e9b12a617b54d6478fa2
Nipunn Koorapati <nipunn@convex.dev>
2026-08-18 10:26:27 -0700

    Add telemetry false to nuxt config. (#56367)
    GitOrigin-RevId: 066e8801928220ab3c78f9785e157f45e0b4cf8b

--- npm-packages/private-demos/quickstarts/nuxt/nuxt.config.ts
+++ npm-packages/private-demos/quickstarts/nuxt/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  telemetry: false,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['convex-nuxt'],
