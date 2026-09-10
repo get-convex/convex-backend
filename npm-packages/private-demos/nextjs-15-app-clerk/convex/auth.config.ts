28c421cfe56ce555a2e8a49a56aa4d7a8fa946fc
Nicolas Ettlin <nicolas@convex.dev>
2026-09-10 13:07:00 -0700

    process.env → env in monorepo: Update nextjs-15-app-clerk (#57506)
    GitOrigin-RevId: 508a35f6d73fe5f83bb726801bdfce6cda3c38d1

--- npm-packages/private-demos/nextjs-15-app-clerk/convex/auth.config.ts
+++ npm-packages/private-demos/nextjs-15-app-clerk/convex/auth.config.ts
@@ -1,13 +1,13 @@
 import { AuthConfig } from "convex/server";
+import { env } from "./_generated/server";
 
 export default {
   providers: [
     {
-      // Replace with your own Clerk Issuer URL from your "convex" JWT template
-      // or with `process.env.CLERK_JWT_ISSUER_DOMAIN`
-      // and configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
+      // Set CLERK_JWT_ISSUER_DOMAIN on the Convex dashboard to the Issuer URL
+      // of your "convex" JWT template.
       // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN!,
+      domain: env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
