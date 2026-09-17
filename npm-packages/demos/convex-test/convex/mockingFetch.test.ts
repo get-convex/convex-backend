251a155d5275928e93d6f24220a2c0d8a3a89652
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-09-17 15:33:14 -0700

    Revert "discourage stubGlobals" (#57774)
    GitOrigin-RevId: 078b1d3f08a4510f9d20fb8f9a2466c2ab147c15

--- npm-packages/demos/convex-test/convex/mockingFetch.test.ts
+++ npm-packages/demos/convex-test/convex/mockingFetch.test.ts
@@ -6,19 +6,15 @@ import { convexTest } from "convex-test";
 test("ai", async () => {
   const t = convexTest(schema, modules);
 
-  const originalFetch = globalThis.fetch;
-  globalThis.fetch = vi.fn(
-    async () => ({ text: async () => "I am the overlord" }) as Response,
+  vi.stubGlobal(
+    "fetch",
+    vi.fn(async () => ({ text: async () => "I am the overlord" }) as Response),
   );
 
-  try {
-    const reply = await t.action(api.messages.sendAIMessage, {
-      prompt: "hello",
-    });
-    expect(reply).toEqual("I am the overlord");
-  } finally {
-    globalThis.fetch = originalFetch;
-  }
+  const reply = await t.action(api.messages.sendAIMessage, { prompt: "hello" });
+  expect(reply).toEqual("I am the overlord");
+
+  vi.unstubAllGlobals();
 });
 
 const modules = import.meta.glob("./**/*.ts");
